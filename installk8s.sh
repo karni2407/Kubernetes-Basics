e42bd74e3d86effb51ae0b1f9e21382d75bdeb3c
Karni Singh <karnisingh2407@gmail.com>
2026-08-08 10:43:24 +0000

    Added fixes

--- installk8s.sh
+++ installk8s.sh
@@ -5,6 +5,11 @@ set -e
 echo "Updating packages..."
 sudo apt update
 
+echo "Installing Docker..."
+sudo apt install -y docker.io
+sudo systemctl enable --now docker
+sudo usermod -aG docker $USER
+
 echo "Installing kubectl..."
 curl -LO "https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
@@ -15,8 +20,12 @@ curl -Lo kind https://kind.sigs.k8s.io/dl/latest/kind-linux-amd64
 chmod +x kind
 sudo mv kind /usr/local/bin/
 
-echo "Verifying installation..."
-kubectl version --client
-kind --version
+echo "Applying Docker group permissions..."
+newgrp docker
 
 echo "Done!"
+
+echo "Versions:"
+docker --version
+kubectl version --client
+kind --version
