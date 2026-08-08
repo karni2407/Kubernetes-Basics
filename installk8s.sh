61df3c8a26c7a21ac69bafe806ea90943f8b9ddf
Karni Singh <karnisingh2407@gmail.com>
2026-08-08 10:41:16 +0000

    Added installation shell script for kubernetes

---
+++ installk8s.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+echo "Updating packages..."
+sudo apt update
+
+echo "Installing kubectl..."
+curl -LO "https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+echo "Installing Kind..."
+curl -Lo kind https://kind.sigs.k8s.io/dl/latest/kind-linux-amd64
+chmod +x kind
+sudo mv kind /usr/local/bin/
+
+echo "Verifying installation..."
+kubectl version --client
+kind --version
+
+echo "Done!"
